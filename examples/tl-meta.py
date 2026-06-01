dcc790e74c5ac47fc81d101b0c1c0010220423bc
Steven Nguyen <nguyen@twinleaf.com>
2026-06-01 12:55:42 -0400

    chore(examples): clean up imports

--- examples/tl-meta.py
+++ examples/tl-meta.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import pprint
+
 import twinleaf
 
 dev = twinleaf.Device()
 
 meta = dev._get_metadata()
-import pprint
 pp = pprint.PrettyPrinter(indent=1)
 pp.pprint(meta)
--- examples/tl-samples.py
+++ examples/tl-samples.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import twinleaf
-import pprint
 
 dev = twinleaf.Device()
 
